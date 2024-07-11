a618cd44be91bbb039461f49ae55c1504090773d
DESIREAK001 <139148791+DESIREAK001@users.noreply.github.com>
2024-07-11 10:23:45 +0300

    Add files via upload

---
+++ DESIREKABAGYO.ino
@@ -0,0 +1,26 @@
+const int led1 = 4;
+const int led2 = 5;
+const int led3 = 13;
+
+void setup(){
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  Serial.begin(15200);
+  Serial.println("Iniatialising program");
+}
+
+void loop(){
+  digitalWrite(led1, HIGH);
+  delay(500);
+  digitalWrite(led1, LOW);
+  delay(500);
+  digitalWrite(led2, HIGH);
+  delay(500);
+  digitalWrite(led2, LOW);
+  delay(500);
+  digitalWrite(led3, HIGH);
+  delay(500);
+  digitalWrite(led3, LOW);
+  delay(500);
+}
