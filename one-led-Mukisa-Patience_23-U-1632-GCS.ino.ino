3814234095903cb49c2543c9353c17d669d873d9
Patience-M <patiencecarol19@gmail.com>
2024-07-19 10:31:27 +0300

    Add files via upload

---
+++ one-led-Mukisa-Patience_23-U-1632-GCS.ino.ino
@@ -0,0 +1,15 @@
+//assigning the pins to the different component
+const int ledPin = 13;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
+  delay(300);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
